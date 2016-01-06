5e4682c885d715c70803d6630f9d0a263e42266c
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-01-06 12:52:31 -0600

    Added my first get view and created my first file to redirect to

---
+++ app/views/roll-dice.php
@@ -0,0 +1,7 @@
+<?php
+
+// Add a view named roll-dice.php. Instead of just returning the random number, show the view and have it display the random number.
+// Modify the route to take in a parameter named guess. Someone will access the route by visiting http://blog.dev/rolldice/1, where 1 is their guess.
+// Modify the route and view so that you can display the guess in addition to the roll and also tell if the guess matches the roll.
+
+?>
